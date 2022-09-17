b6582cb66ae047cc31e80a90ee8e2f0ca6eb92f1
Nishit Shah <ngshah3@myseneca.ca>
2022-09-17 19:57:27 -0400

    done with .h file

--- WORKSHOP - 01/part 1/carads.h
+++ WORKSHOP - 01/part 1/carads.h
@@ -26,7 +26,32 @@ namespace sdds {
 	//listArgs receives the same parameters as the main function (same types, same order)
 	void listArgs(int argc, char* argv[]);
 
+	//Class Cars
+	class Cars {
+		//a C-style null-terminated string of up to 10 characters including the null byte terminator representing the brand of the car.
+		char* m_brand{};
+		//a C-style null-terminated string of up to 15 characters including the null byte terminator representing the model of the car.
+		char* m_model{};
+		//the manufacture year of the car.
+		int m_year{};
+		//the price of the car represented by a double value.
+		double m_price{};
+		//the status of the cars as used or new using a single character (holds N or U)
+		char m_status{};
+		//a boolean flag indicating whether or not the car is on promotion discount
+		bool m_onPromotion{};
+		size_t m_counter{};
 
+	public:
+		//default constructor
+		Cars();
+		//reads from istream
+		void read(std::istream& is);
+		//displays to the screen the content of an Cars instance
+		void display(bool reset) const;
+		//returns if the car is new or used.
+		char getStatus() const;
+	};
 }
 
 #endif
