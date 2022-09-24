b60ce99c28b384020a643e3523e602a9061cf403
Nishit Shah <ngshah3@myseneca.ca>
2022-09-24 10:03:07 -0400

    TennisLog.h created

---
+++ WORKSHOP - 02/part 1/TennisLog.h
@@ -0,0 +1,23 @@
+
+/*************************************************************************
+Workshop 2
+Full Name   : Nishit Gaurang Shah
+Student ID# : 130 176 217
+Email       : ngshah3@myseneca.ca
+Section     : OOP345 NCC
+Date : 24th September 2022
+Autheticity Declaraition :
+I declare that I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and assignments.
+*************************************************************************/
+
+//including the necessary guards in the header file
+#pragma once
+#ifndef SDDS_TENNISLOG_H_
+#define SDDS_TENNISLOG_H_
+
+namespace sdds {
+	// hello;
+}
+
+#endif // !SDDS_TENNISLOG_H_
